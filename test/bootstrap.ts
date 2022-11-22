1bb70f3ee15a1f39eaaad4690541d86319c062b1
barry hayles <barry.hayles@lytical.com>
2022-11-22 15:47:26 -0500

    minor change to unit test

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -13,9 +13,8 @@ before(() => {
 describe('an app instance', () => {
   it('can bootstrap the application', async () => {
     var x = new lmvc_app();
-    expect(x).is.not.undefined;
+    expect(x, 'failed to allocate app.').is.not.undefined;
     x.bootstrap();
-    expect(true).to.be.true;
-    expect(window.document.querySelector('#lmvc-test-controller')).is.not.null;
+    expect(window.document.querySelector('#lmvc-test-controller'), 'the test controller did not replace the dom element.').is.not.null;
   });
 });
