e82d56301c6f5d567a9ed92062d5186dba7186e2
barry hayles <barry.hayles@lytical.com>
2022-12-30 08:37:20 -0500

    finished l:class unit test

--- test/class.ts
+++ test/class.ts
@@ -15,15 +15,22 @@ describe('l:class view', () => {
       let y = window.document.querySelector('#test-class');
       expect(y, 'the test element is not in the dom.').is.not.null;
       if(y) {
-        // test_app.$model.text = 'show';
+        expect(y.classList).is.empty;
+        test_app.$model.text = 'foobar';
         await fixture.timeout(1);
-        // expect(y.closest('body')).is.not.null;
-        // test_app.$model.text = 'hide';
-        // await fixture.timeout(1);
-        // expect(y.closest('body')).is.null;
-        // test_app.$model.text = 'show';
-        // await fixture.timeout(1);
-        // expect(y.closest('body')).is.not.null;
+        expect(y.classList).is.empty;
+        test_app.$model.text = 'mute';
+        await fixture.timeout(1);
+        expect(y.classList).length(1);
+        expect(y.classList.contains('text-muted')).is.true;
+        test_app.$model.text = 'space';
+        await fixture.timeout(1);
+        expect(y.classList).length(2);
+        expect(y.classList.contains('bt-4')).is.true;
+        expect(y.classList.contains('pb-2')).is.true;
+        test_app.$model.text = 'foobar';
+        await fixture.timeout(1);
+        expect(y.classList).is.empty;
         y.parentNode?.removeChild(y);
       }
     }
