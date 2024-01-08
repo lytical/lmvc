d4a7f3797b9ccb87115dd253f6cf00f08bbef8df
Barrington O Hayles <BarringtonH@newsmax.com>
2024-01-08 14:02:39 -0500

    updated build version

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,16 @@
   please refer to your license agreement on the use of this file.
 */
 
+const _del = require('del');
 const _gulp = require('gulp');
 const _pump = require('pump');
 const _uglify = require('gulp-uglify-es').default;
 
-exports.post_build = _gulp.parallel
+exports.post_build = _gulp.series(
+  _gulp.parallel
   (
     done => _pump(_gulp.src(['package.json', 'README.md']), _gulp.dest('../.dist'), done),
     done => _pump(_gulp.src('../.dist/**/*.js', { dot: true }), _uglify({ mangle: { keep_fnames: true }, output: { comments: 'some' } }), _gulp.dest('../.dist'), done)
-  );
+  ),
+  done => { _del(['../.dist/*.{map,tsbuildinfo}'], { force: true }); done() }
+);
